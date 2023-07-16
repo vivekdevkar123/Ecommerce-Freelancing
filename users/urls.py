c920ce83d693c73ec16bae46f91585535b0164ca
vivek devkar <mrvivekdevkar123@gmail.com>
2023-07-16 23:11:32 +0530

    otp verification added

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup', signup),
-    path('login', loginPage, name='login'),
+    path('signup/', signup, name='signup'),
+    path('login/', loginPage, name='login'),
     path('logout/', logoutUser, name='logout'),
+    path('OtpVerification/', OtpVerification, name='OtpVerification'),
 ]
